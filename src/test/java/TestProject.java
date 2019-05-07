18b1253700fc1f9af3353a9f74c73ba9e2baf312
ealtunhan <ealtunhan@gmail.com>
2019-05-07 15:39:18 -0400

    last changed

--- src/test/java/TestProject.java
+++ src/test/java/TestProject.java
@@ -5,7 +5,9 @@ public class TestProject {
         System.out.println("it is the first time that I came here");
         System.out.println("it is the second time that I came here");
         System.out.println("it is the third time that I came here");
-        System.out.println("it is the forth time that I came here");
+        System.out.println("it is the fourth time that I came here");
+        System.out.println("it is the seventh time that I came here");
+        System.out.println("it is the eighth time that I came here");
     }
 
 }
